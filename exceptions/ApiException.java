ca212bd3a311915119e5e674950155719a491102
Kareem Ezzat <Kareemezzat.125@gmail.com>
2023-11-08 14:19:09 +0200

    App Commit

---
+++ exceptions/ApiException.java
@@ -0,0 +1,14 @@
+package com.ke.backend.exceptions;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+public record ApiException(
+        String message,
+        @JsonIgnore(value = true) Throwable throwable,
+        String httpStatus,
+        int httpStatusCode, ZonedDateTime zonedDateTime
+) { }
